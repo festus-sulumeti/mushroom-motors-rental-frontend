9093c70e8fa1ec9c84c0c0504d2884ed2e3b09e3
festus-sulumeti <sulumetifestus@outlook.com>
2025-07-22 14:36:32 +0300

    feat:added the account settings link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './components/Pages/LandingPage';
 import  LoginPage from './components/auth/Login';
 import SignUpPage from './components/auth/Signup';
 import UserDashboardHome from './components/user-dashboard/UserDashboardHome';
+import AccountSettings from './components/user-dashboard/AccountSettings';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/dashboard" element={<UserDashboardHome />} />
+        <Route path="/account-settings" element={<AccountSettings />} />
       </Routes>
     </Router>
   );
